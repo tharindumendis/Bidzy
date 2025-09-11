9607a35c6c964c44ae149824c5e009e901d6af4e
Tharindu Dahampriya Mendis <tharindumendis100@outlook.com>
2025-09-11 23:58:18 +0530

    refactor: jobscheduler

---
+++ Bidzy/Application/Services/Scheduler/IJobScheduler.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+
+namespace Bidzy.Application.Services.Scheduler
+{
+    public interface IJobScheduler
+    {
+        void Enqueue<T>(Expression<Action<T>> methodCall);
+        void Schedule<T>(Expression<Action<T>> methodCall, TimeSpan delay);
+        void Recurring<T>(string jobId, Expression<Action<T>> methodCall, string cronExpression);
+        
+    }
+
+}
